5675136df1887230aab45b8e7af301f0de8d9032
Jahanave <meesala.jahanave@sap.com>
2026-07-21 23:29:37 +0530

    Update server port for deployment

--- server/server.js
+++ server/server.js
@@ -36,7 +36,7 @@ pool.query("SELECT NOW()", (err, result) => {
   }
 });
 
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
